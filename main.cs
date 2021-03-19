238dd0306d49f5491f4aced18b474e3eda66997a
awesomearjun <64511698+awesomearjun@users.noreply.github.com>
2021-03-18 21:40:52 -0400

    Add files via upload

---
+++ main.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Calc
+{
+    class Calc
+    {
+        static void Main(string[] args) {
+            Console.WriteLine("Enter the first number:\n");
+            double firstNum = Console.WriteLine();
+        }
+    }   
+}
